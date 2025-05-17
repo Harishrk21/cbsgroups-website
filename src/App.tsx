5c93c4140320c97914ed958e34f854c4fae26c91
Harishrk21 <harishradhakrishnan2001@gmail.com>
2025-05-17 20:50:52 +0530

    Moved to production

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import LoanPage from './pages/LoanPage';
+import BlogPage from './pages/BlogPage';
+import BlogDetailPage from './pages/BlogDetailPage';
+import ScrollToTop from './components/common/ScrollToTop';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Layout>
+      <main className="pt-[15px]"> 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/loans/:loanType" element={<LoanPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:blogId" element={<BlogDetailPage />} />
+        </Routes>
+        </main>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
